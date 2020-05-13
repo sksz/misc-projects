533a3a409df584cfe164bf85010e94f7fe41a10f
Ann Herbik <45009615+ann-dev@users.noreply.github.com>
2020-05-13 19:27:05 +0200

    Car speed setter

--- eduweb-php/index.php
+++ eduweb-php/index.php
@@ -4,6 +4,7 @@ include_once __DIR__ . '/vendor/autoload.php';
 use \Eduweb\Car;
 
 $car = new Car('Toyota', 'Corolla');
+$car->setSpeed(50.6);
 
 var_dump($car);
 
--- eduweb-php/src/Car.php
+++ eduweb-php/src/Car.php
@@ -6,12 +6,21 @@ class Car
 {
     protected $maker = '';
     protected $model = '';
-    protected $speed = 0;
+    protected $speed = 0.0;
 
     public function __construct(string $maker, string $model)
     {
         $this->maker = $maker;
         $this->model = $model;
+    }
+
+    public function setSpeed(float $speed): void
+    {
+        if($speed < 0) {
+            return;
+        }
+
+        $this->speed = $speed;
     }
 
     public function drive(): void
