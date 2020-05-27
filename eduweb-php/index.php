644de71d81a597fcf75ceb011865a9db845bb989
Ann Herbik <45009615+ann-dev@users.noreply.github.com>
2020-05-27 15:35:58 +0200

    eduweb-php: SportsCar decorator

--- eduweb-php/index.php
+++ eduweb-php/index.php
@@ -4,8 +4,8 @@ include_once __DIR__ . '/vendor/autoload.php';
 
 use \Eduweb\Game\Race;
 use \Eduweb\Game\Weather;
-use \Eduweb\Game\VehicleFactory;
 use \Eduweb\Game\VehicleBuilder;
+use \Eduweb\Game\SportsCar;
 
 $builder = new VehicleBuilder();
 
@@ -14,18 +14,20 @@ $weather = new Weather();
 $race = new Race($weather);
 
 $builder->setType(VehicleBuilder::CAR);
+
 $builder->setName('Fiat');
-$builder->build();
 $race->addVehicle($builder->build());
 
-$builder->setName('Toyota');
-$race->addVehicle($builder->build());
+$builder->setName('Bugatti');
+$race->addVehicle(new SportsCar($builder->build()));
 
 $builder->setType(VehicleBuilder::TRUCK);
+
 $builder->setName('Man');
 $race->addVehicle($builder->build());
 
 $builder->setType(VehicleBuilder::BIKE);
+
 $builder->setName('Yamaha');
 $race->addVehicle($builder->build());
 
---
+++ eduweb-php/src/game/SportsCar.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Eduweb\Game;
+
+class SportsCar implements Vehicle
+{
+    /**
+     * @var Car
+     */
+    private $car;
+
+    public function __construct(Vehicle $car)
+    {
+        $this->car = $car;
+    }
+
+    public function move(): void
+    {
+        ob_start();
+        $this->car->move();
+        ob_end_clean();
+
+        echo sprintf("\n I'm moving (%s) - %02.1f", $this->car->getName(), $this->car->getDistance());
+    }
+
+    public function getDistance(): float
+    {
+        return $this->car->getDistance();
+    }
+
+    public function getName(): string
+    {
+        return '__' . $this->car->getName();
+    }
+
+    public function getType(): string
+    {
+        return $this->car->getType();
+    }
+}
